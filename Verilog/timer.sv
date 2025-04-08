fa7d53be950c083832fcf56cc24e4ca305c26562
jneau <jneau@ad.engr.wisc.edu>
2025-04-08 17:22:25 -0500

    add more to state machines

--- Verilog/timer.sv
+++ Verilog/timer.sv
@@ -2,14 +2,26 @@ module timer
 # (
     parameter clock_frequency_mhz   = 50;
     parameter time_milliseconds     = 1000;
-    parameter is_oneshot            = 1;
-    parameter is_autostart          = 0;
 ) (
     input clk,
     input rst_n,
     input stop,
     input start,
-    output 
+    output done
 );
+
+// Intermediate Signals
+logic 
+
+// State Machine Signals
+logic [2:0] {IDLE, RUN} state, state_next;
+
+// State Machine Sequential Logic
+always_ff @(posedge clk or negedge rst_n) begin
+    state = state_next;
+end
+
+// State Machine implementation
+
 
 endmodule
