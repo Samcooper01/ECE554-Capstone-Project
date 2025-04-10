ebfe3a60901ad250c1dbc01fd46c8c6e0bc1df36
jneau <jneau@ad.engr.wisc.edu>
2025-04-10 18:30:24 -0500

    implement tracking buffer

--- Verilog/state_machine_tb.sv
+++ Verilog/state_machine_tb.sv
@@ -4,58 +4,53 @@ module state_machine_tb ();
 logic           clk;
 logic           rst_n;
 logic           on_screen;
-logic  [10:0]   tracked_coordinates_x,
-logic  [10:0]   tracked_coordinates_y,
-logic  [10:0]   driven_coordinates_x,
-logic  [10:0]   driven_coordinates_y,
-logic           fire
+logic  [10:0]   tracked_coordinates_x;
+logic  [10:0]   tracked_coordinates_y;
+logic  [10:0]   driven_coordinates_x;
+logic  [10:0]   driven_coordinates_y;
+logic           fire;
 
 // DUT
 state_machine #(
-    parameter clock_frequency_mhz       = 50,
-    parameter fire_time_milliseconds    = 200
-)
-
-(
-    input           clk,
-    input           rst_n,
-    input           on_screen,              // set when the paper airplaine is on screen calculated by the object tracker
-    input   [10:0]  tracked_coordinates_x,  // coordinates of the tracked paper airplane
-    input   [10:0]  tracked_coordinates_y,
-    output  [10:0]  driven_coordinates_x,   // coordinates given to the servo pipeline
-    output  [10:0]  driven_coordinates_y,
-    output          fire                    // turn this on to fire laser
+    .clock_frequency_mhz(1),
+    .fire_time_milliseconds(1)
+) DUT (
+    .clk(clk),
+    .rst_n(rst_n),
+    .on_screen(on_screen),
+    .tracked_coordinates_x(tracked_coordinates_x),
+    .tracked_coordinates_y(tracked_coordinates_y),
+    .driven_coordinates_x(driven_coordinates_x),
+    .driven_coordinates_y(driven_coordinates_y),
+    .fire(fire)
 );
 
 initial begin
     clk = 0;
     rst_n = 0;
-    start = 0;
-    stop = 0;
+    on_screen = 0;
+    tracked_coordinates_x = 0;
+    tracked_coordinates_y = 0;
     @(negedge clk);
     rst_n = 1;
     @(negedge clk);
-
-    $display("Test 1: Ensure time stops and done goes low one cycle after end of timer");
-    start = 1;
-    @(negedge clk);
-    start = 0;
-    @(posedge done);
-    repeat (2) @(negedge clk);
-    if (done !== 0) begin
-        $display("Done did not go low");
+    on_screen = 1;
+    repeat(1010) @(negedge clk);
+    if (fire !== 1) begin
+        $display("Error: fire signal failed to go high after timer should have ended");
         $stop();
     end
+    on_screen = 0;
+    repeat(10) @(negedge clk);
 
-    @(negedge clk);
-    start = 1;
-    @(negedge clk);
-    start = 0;
-    @(negedge clk);
-    stop = 1;
-    @(negedge clk);
-    stop = 0;
-    repeat (1000) @(negedge clk);
+    on_screen = 1;
+    repeat(505) @(negedge clk);
+    if (fire !== 0) begin
+        $display("Error: fire signal went high too early");
+        $stop();
+    end
+    on_screen = 0;
+    repeat(10) @(negedge clk);
 
     $display("YAHOO!!! All tests passed!");
     $stop();
---
+++ Verilog/tracking_buffer.sv
@@ -0,0 +1,31 @@
+module tracking_buffer # (
+    parameter clock_frequency_mhz       = 50,
+    parameter buffer_time_milliseconds    = 1000
+)
+(
+    input           clk,
+    input           rst_n,
+    input           oObjectDetected,
+    output          on_screen
+);
+
+// Intermediate Signals
+logic timer_done;
+logic on_screen_intermediate;
+
+// Instantiated Modules
+timer #(.clock_frequency_mhz(clock_frequency_mhz), .time_milliseconds(buffer_time_milliseconds)) 
+    fire_timer (.clk(clk), .rst_n(rst_n), .start(!oObjectDetected), .stop(oObjectDetected), .done(timer_done));
+
+// Logic
+assign on_screen = on_screen_intermediate;
+always @(oObjectDetected, timer_done, rst_n) begin
+    if (oObjectDetected) begin
+        on_screen_intermediate = 1;
+    end
+    else if (timer_done || !rst_n) begin
+        on_screen_intermediate = 0;
+    end
+end
+
+endmodule
