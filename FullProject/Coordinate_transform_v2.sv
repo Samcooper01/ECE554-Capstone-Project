2c6a64adbd70ee3660e38679777675b6e9ef7490
jneau <jneau@ad.engr.wisc.edu>
2025-04-14 15:07:03 -0500

    move files for full project

---
+++ FullProject/Coordinate_transform_v2.sv
@@ -0,0 +1,34 @@
+
+module Coordinate_transform_v2 #(
+    parameter pan_FOV = 45,
+    parameter tilt_FOV = 30
+	//assumes 640 x 480, hardcoded
+    //parameter x_res = 640,
+    //parameter y_res = 480
+    )
+	(input signed [9:0] x,
+	 input signed [8:0] y,
+     input clk, rst_n,
+     output reg [10:0] pan, tilt);
+
+    // Find # of us that is in the FOV (~FOV*11.111...)
+    logic signed[9:0]available_pan, available_tilt;
+    assign available_pan = (pan_FOV * 14'd11111) / 10'd1000; 
+    assign available_tilt = (tilt_FOV * 14'd11111) / 10'd1000;
+
+	// Center x, y on grid for calculations (assumes top left is 0,0), accounts for servo install
+    logic signed [10:0] x_adj;
+    logic signed [9:0] y_adj;
+    logic signed [20:0] x_mult, y_mult;
+    logic signed [31:0] x_div, y_div;
+  always_ff @(posedge clk) begin
+    x_adj <= x - 10'd320;
+    y_adj <= y - 10'd240;
+    x_mult <= x_adj * available_pan;
+    y_mult <= y_adj * available_tilt;
+    x_div <= (x_mult * 205) >>> 17;
+    y_div <= (y_mult * 4369) >>> 21;
+    pan <=  x_div + available_pan;
+    tilt <= y_div + available_tilt;
+  end
+endmodule
---
+++ FullProject/state_machine.sv
@@ -0,0 +1,74 @@
+module state_machine # (
+    parameter clock_frequency_mhz       = 50,
+    parameter fire_time_milliseconds    = 200
+)
+(
+    input           clk,
+    input           rst_n,
+    input           on_screen,              // set when the paper airplaine is on screen calculated by the object tracker
+    input   [9:0]  tracked_coordinates_x,  // coordinates of the tracked paper airplane
+    input   [8:0]  tracked_coordinates_y,
+    output  [9:0]  driven_coordinates_x,   // coordinates given to the servo pipeline
+    output  [8:0]  driven_coordinates_y,
+    output          fire                    // turn this on to fire laser
+);
+
+// Intermediate Signals
+logic timer_start;
+logic timer_stop;
+logic timer_done;
+
+// Instantiated Modules
+timer #(.clock_frequency_mhz(clock_frequency_mhz), .time_milliseconds(fire_time_milliseconds)) 
+    fire_timer (.clk(clk), .rst_n(rst_n), .start(timer_start), .stop(timer_stop), .done(timer_done));
+
+// State Machine Signals
+enum logic [2:0] {IDLE, TRACKING, LOCKED} state, state_next;
+
+// State Machine Sequential Logic
+always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        state = IDLE;
+    end
+    else begin
+        state = state_next;
+    end
+end
+
+// State Machine implementation
+always_comb begin
+    state_next = state;
+    case (state)
+        IDLE: begin
+            if (on_screen) begin
+                state_next = TRACKING;
+            end
+        end
+        TRACKING: begin
+            if (!on_screen) begin
+                state_next = IDLE;
+            end
+            else if (timer_done) begin
+                state_next = LOCKED;
+            end
+        end
+        LOCKED: begin
+            if (!on_screen) begin
+                state_next = IDLE;
+            end
+        end
+        default: begin
+            state_next = IDLE;
+        end
+
+    endcase
+end
+
+// State machine outputs
+assign driven_coordinates_x = (state == IDLE) ? 320 : tracked_coordinates_x;
+assign driven_coordinates_y = (state == IDLE) ? 240 : tracked_coordinates_y;
+assign fire = (state == LOCKED) ? 1 : 0;
+assign timer_start = (state == TRACKING) ? 1 : 0;
+assign timer_stop = !on_screen;
+
+endmodule
