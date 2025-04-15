e838a7b98cb9ce05fc5356046fb253ffe588adce
jneau <jneau@ad.engr.wisc.edu>
2025-04-15 17:20:49 -0500

    retry pixel painting

--- FullProject/v/RAW2RGB_640X480.sv
+++ FullProject/v/RAW2RGB_640X480.sv
@@ -88,7 +88,7 @@ assign	oDVAL	=	mDVAL;
 // assign oBlue	= 	({iY_Cont[0], iX_Cont[0]} == 2'b01) ? iDATA : '0;
 // assign oDVAL 	= 	iDVAL;
 
-assign isWithinThreshold = ((iX_Cont >= tracked_coordinates_x - 30 && iX_Cont <= tracked_coordinates_x + 30) && 
+assign isWithinThreshold = ((iX_Cont >= tracked_coordinates_x - 30 && iX_Cont <= tracked_coordinates_x + 30) &&
 	(iY_Cont >= tracked_coordinates_y - 30 && iY_Cont <= tracked_coordinates_y + 30));
 
 assign oRed 	= 	(isWithinThreshold) ? 12'hFFF : iDATA;
